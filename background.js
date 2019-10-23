e923bf07a375b181ea23be5c63b72238be466efc
wert2401 <ilya.f.s01@mail.ru>
2019-10-24 01:15:14 +0700

    Correct UI and cool timeouts

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
 chrome.runtime.onMessage.addListener(function (message, sender, reply) {
+    const time = ((message.id + 2) % 7 == 1) ? 500 : 300;
     if (message.itemName) {
         setTimeout(() => {
             fetch("https://steamcommunity.com/market/search/render/?norender=1&query=" + message.itemName)
             .then(response => response.json())
             .then(json => reply(json))
             .catch(reason => reply(reason))
-        }, 1000*message.id);
+        }, time * message.id);
     }
     return true;
 })
--- market.js
+++ market.js
@@ -4,23 +4,29 @@ document.onreadystatechange = function(){
         for (let i = 0; i < items.length; i++) {
             const element = items[i];
             var name = element.getElementsByClassName("name")[0].innerHTML;
-
-            chrome.runtime.sendMessage({itemName: name.replace(' ', '%20'), id: i/2}, price => {
-                var priceElement = element.getElementsByClassName("imageblock")[0].getElementsByClassName("price")[0];
+            const imageblock = element.getElementsByClassName('imageblock')[0];
+            
+            chrome.runtime.sendMessage({itemName: name.replace(' ', '%20'), id: i}, price => {
+                var priceElement = imageblock.getElementsByClassName("price")[0];
+                const steamPriceNum = JSON.stringify(price.results[0].sale_price_text).replace('"', '').replace('"', '').replace(' pуб.', '').replace(',', '.');
+                const percent = (((steamPriceNum / parseFloat(priceElement.innerHTML.replace('&nbsp', '').replace(' ', '').replace(';', ''))) - 1)*100);
                 priceElement.setAttribute("style",  "margin-bottom: 30px;");
 
                 var steamPrice = document.createElement('div');
                 steamPrice.className = "price";
                 steamPrice.setAttribute("style", "color: #00dcff; margin-bottom: 15px")
-                element.getElementsByClassName('imageblock')[0].append(steamPrice);
-                steamPrice.innerHTML = JSON.stringify(price.results[0].sale_price_text);
+                imageblock.append(steamPrice);
+                steamPrice.innerHTML = JSON.stringify(price.results[0].sale_price_text).replace('"', '').replace('"', '');
 
                 steamPrice = document.createElement('div');
                 steamPrice.className = "price";
-                element.getElementsByClassName('imageblock')[0].append(steamPrice);
-                const steamPriceNum = JSON.stringify(price.results[0].sale_price_text).replace('"', '').replace('"', '').replace(' pуб.', '');
-                steamPrice.innerHTML = steamPriceNum / priceElement.innerHTML.replace(' ', '');
-                steamPrice.setAttribute("style", "color: rgb(155, "+ 255 * steamPriceNum  +", 102)");
+                if (percent > 0) {
+                    steamPrice.setAttribute("style", "color: rgb(0, "+((255 * percent/100) + 100) +", 0)");
+                } else {
+                    steamPrice.setAttribute("style", "color: rgb("+((255 * -percent/100) + 150) +", 0, 0)");                    
+                }
+                imageblock.append(steamPrice);
+                steamPrice.innerHTML = (((steamPriceNum / parseFloat(priceElement.innerHTML.replace('&nbsp', '').replace(' ', '').replace(';', ''))) - 1)*100).toFixed(2) + ' %';
             })
         }
     }
